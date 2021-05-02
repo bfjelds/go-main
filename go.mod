e254edcc0da415cae71d4090d29766bc1e1dc1b5
Brian Fjeldstad <bfjelds@microsoft.com>
2021-05-02 08:33:49 -0700

    fix versioning, ref go-library tag

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bfjelds/go-main
 
 go 1.14
 
-require github.com/bfjelds/go-library v0.0.0-20210502151646-81c0042e2dcd
+require github.com/bfjelds/go-library v0.0.3-alpha1
--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/bfjelds/go-library/pkg/version"
+	bfjeldsversion "github.com/bfjelds/go-library/pkg/version"
+)
+
+var (
+	version        string
+	libraryversion string
+	commit         string
+	date           string
 )
 
 func main() {
-	fmt.Printf("library version: %v\n", version.GetVersion())
+	fmt.Printf("library version: %v\n", bfjeldsversion.GetVersion())
+	fmt.Printf("Version:\n")
+	fmt.Printf("    go-library: %v\n", libraryversion)
+	fmt.Printf("    go-main:    %v\n", version)
+	fmt.Printf("    commit:     %v\n", commit)
+	fmt.Printf("    date:       %v\n", date)
 }
